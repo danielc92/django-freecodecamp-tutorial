c2ed370c95c4ccdf8d719d183dff6d9334455eea
Daniel Corcoran <danielc.92@outlook.com>
2019-05-07 10:58:27 +1000

    Updated product template to include product query object

--- products/views.py
+++ products/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def products_view(request, *args, **kwargs):
     print(request.headers)
     print(request.user)
     print(request.method)
-    return HttpResponse('<h1>Products View</h1>')
+    # return HttpResponse('<h1>Products View</h1>')
+
+    data = Product.objects.all()
+
+    context = {'title':'Product Page',
+    		   'data':data}
+
+    return render(request, 'products.html', context)
